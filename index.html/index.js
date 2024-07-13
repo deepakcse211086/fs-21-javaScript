55b8064837021acdb5fc875befa536e626779db0
Deepak kumar singh <dksingh123skd6653>
2024-07-13 20:46:12 +0530

    add

---
+++ index.html/index.js
@@ -0,0 +1,13 @@
+const data = prompt("Give a number");
+
+// alert(data);
+// alert(345);
+
+console.log(data);
+
+console.log(typeof data);
+console.log("we are here");
+
+const num = parseInt(data);
+console.log(num);
+console.log(typeof num);
