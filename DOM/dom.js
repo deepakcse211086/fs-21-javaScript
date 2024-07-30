dc34f9a9b73be5c8026eeba91a61509568f80570
Deepak kumar singh <dksingh123skd6653>
2024-07-30 20:33:33 +0530

    index

---
+++ DOM/dom.js
@@ -0,0 +1,16 @@
+document.getElementById('yourTargetDivId');
+
+targetDiv.style.backgroundColor = 'yellow';
+
+targetDiv.style.margin = '20px';
+
+targetDiv.style.padding = '10px';
+
+targetDiv.style.fontSize = '18px';
+
+targetDiv.style.fontWeight = 'bold';
+
+targetDiv.style.height = '200px';
+
+targetDiv.style.width = '300px';
+
