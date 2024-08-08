64d0ab0fdd8a699398eb21d1209186184c6a0cd4
Deepak kumar singh <dksingh123skd6653>
2024-08-08 22:16:12 +0530

    Leader board Projject

--- Generating the keyboard/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('keydown', function(event) {
-    // Get the key and keycode values
-    const key = event.key;
-    const keycode = event.keyCode;
-
-    // Update the UI with the key and keycode values
-    document.getElementById('key').textContent = key;
-    document.getElementById('keycode').textContent = keycode;
-});
